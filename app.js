3b4540253b3c23a04c54aeb2b07c707fdcc11baa
saikumar <nalivela>
2018-10-28 15:16:22 -0500

    Added app.js

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const http = require('http').Server(app)  // inject app into the server
 // 6 respond with 404 if a bad URI is requested
 
 // Listen for an application request on port 8081
-http.listen(8081, function () {
-  console.log('app listening on http://127.0.0.1:8081/')
-})
+// http.listen(8081, function () {
+//   console.log('app listening on http://127.0.0.1:8081/')
+// })
 // 1 set up the view engine
 app.set("views", path.resolve(__dirname, "views")) // path to views
 app.set("view engine", "ejs") // specify our view
@@ -80,7 +80,7 @@ app.get(function (req, res) {
  }) 
  
  // Listen for an application request on designated port
- app.listen(port, function () {
-  console.log('Web app started and listening on http://localhost:' + port)
+ app.listenp(process.env.PORT || 8081, function () {
+  console.log('Web app started and listening on http://localhost:8081' )
  })
 })  
